af28f3d20da1b56bc943e7ea36755e51f68b4af2
matangro <58812754+matangro@users.noreply.github.com>
2021-04-26 16:24:10 +0300

    test

---
+++ rememberGame/ClientApp/src/components/Card.js
@@ -0,0 +1,18 @@
+﻿import React, { Component } from 'react';
+
+export class Card extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { letter:props.letter, found:false, show:false };
+    }
+    clickCard = () => {
+        this.setState({ show: !this.state.show });
+    }
+
+    render() {
+        return (
+            <button className="btn btn-primary" onClick={this.clickCard}>{this.state.letter}{this.state.show ? this.state.letter : null}</button>
+        );
+    }
+}
